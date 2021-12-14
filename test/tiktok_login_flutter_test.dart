d649d662142498655692b0e09c0c2e824f8975e1
Aqueel <aqeeelvp@gmail.com>
2021-12-14 21:30:22 +0530

    Changed futter sdk for 1.22.1

--- test/tiktok_login_flutter_test.dart
+++ test/tiktok_login_flutter_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tiktok_login_flutter/tiktok_login_flutter.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('tiktok_login_flutter');
